e3ae0a9ae6100d4de98e7e43c5f2f2928b8213a7
Shambhu Kumar <shambhu384@gmail.com>
2022-10-21 14:14:45 +0530

    lastest

---
+++ node_modules/@symfony/ux-live-component/src/normalize_attributes_for_comparison.ts
@@ -0,0 +1,26 @@
+/**
+ * Updates an HTML node to represent its underlying data.
+ *
+ * For example, this finds the value property of each underlying node
+ * and sets that onto the value attribute. This is useful to compare
+ * if two nodes are identical.
+ */
+export function normalizeAttributesForComparison(element: HTMLElement): void {
+    const isFileInput = element instanceof HTMLInputElement && element.type === 'file';
+
+    // don't add value attribute to file inputs
+    // if a file is selected, but then a re-render happens before it is
+    // uploaded, we do NOT want to add a value="" attribute because
+    // this would cause the input to re-render (without the attached file)
+    if (!isFileInput) {
+        if ('value' in element) {
+            element.setAttribute('value', (element as HTMLInputElement).value);
+        } else if (element.hasAttribute('value')) {
+            element.setAttribute('value', '');
+        }
+    }
+
+    Array.from(element.children).forEach((child: Element) => {
+        normalizeAttributesForComparison(child as HTMLElement);
+    });
+}
