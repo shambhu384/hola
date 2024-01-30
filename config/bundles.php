ccb68be447a56547a0f6ef82a46c5ee46c139b17
shambhu384@gmail.com <ubuntu@ip-172-31-25-233.ap-south-1.compute.internal>
2024-01-30 16:43:02 +0000

    backup latest

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     CalendarBundle\CalendarBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Typed\TypedBundle::class => ['all' => true],
 ];
